38d84f15a44690635b886417dc8296bb9e01910a
jayson ambrose <jayson.ambrose.87@gmail.com>
2023-03-14 10:17:04 -0600

    stuff

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,22 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
+
+    fib = []
+
+    if length > 0:
+        fib.append(0)
+
+        if length > 1:
+            fib.append(1)
+
+            if length > 2 :
+
+                 while(len(fib) < length):
+                    fib.append(
+                        fib[-1] + fib[-2]
+                    )
+
+    print(fib)
+
     pass
